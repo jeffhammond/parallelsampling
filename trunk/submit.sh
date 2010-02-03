f51fcd04b24a3609206bd9673b92531ba213aa33
alexrossdickson <alexrossdickson@e7e500d4-0c48-11df-8f49-cf7c468afe83>
2010-02-03 21:03:37 +0000

    shell used to submit jobs to surveyor

---
+++ trunk/submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -l nodes=2:ppn=8
+#PBS -q default
+#PBS -N RNAflow
+#PBS -r n
+
+START=$(date +%s)
+
+qsub --env OMP_NUM_THREADS=3 -A ParallelSampling -n 256 --mode smp -q default -t 60 flow_ga.x
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "Total time of job: $DIFF seconds"
