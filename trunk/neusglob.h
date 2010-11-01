abe0b05bc450b95147685da3362afe69960bc441
alexrossdickson <alexrossdickson@e7e500d4-0c48-11df-8f49-cf7c468afe83>
2010-11-01 17:01:35 +0000

    so many changes.
    ww = "weighted walkers".  Now the walkers are carrying weight, and when they get taken off of the fluxlist their weight gets cut in two pieces, one piece stays with the walker, and one stays on the flux list.  
    
    I have found better results when I start with an unbiased ensemble of points obtained from straightforward MD.  These are contained, for p = 0.02, in rstart.dat.  A new rstart file is required for different flow rates to minimize bias.
    
    in ww2, I separated each regions fluxlist into two halves, one devoted to points coming from regions with higher indices, the other for lower indices.  I didn't look in detail how much impact this had on the long-term quality of the results.  Using the larger, unbiased ensemble had the biggest impact, where the quality was measured using comparisons of the transition probabilities (in tprob0.dat, etc.) with straightforward sampling.

--- trunk/neusglob.h
+++ trunk/neusglob.h
@@ -1,11 +1,12 @@
 #define NDIM_c 3
 #define NOP_c 1
+#define MXLIST_c 250
 #define PI_c 3.14159265
-#define NPART_c 64
-#define NEMERG_c 400
+#define NPART_c 262
+#define NEMERG_c 765
 
 const int phase = 2;
-int wtalg = 3;       /* 1 = local ; 2 = global */
+int wtalg = 4;       /* 1 = local ; 2 = global ; 3 = VdE ; 4 = ww-avg */
 const char flname[30] = "NOREAD";
 const char wname[30] = "NOREAD";
 const char fxname[30] = "NOREAD";
@@ -13,29 +14,30 @@ const char pname[30] = "NOREAD";
 const char tname[30] = "NOREAD";
 
 long seed = 1;
-const int beads = 15;
-const int wtupdt = 20000;
-const int T = 1;         /* measured in number of cycles */
-const int every = 20000;
-const int stkfrq = 100;
-const int wrfrq = 20000;
-const int pzero = 500000;
-const int xyzfrq = 200;
-const int globfrq = 5;
-const int chkfrq = 5;  
-const int tres = 100;
+const int beads = 40;
+const int wtupdt = 20;     /* measured in cycles */
+const int T = 2000;         /* measured in number of cycles */
+const int every = 2000;  /* steps */
+const int stkfrq = 100;   /* steps */
+const int wrfrq = 20;      /* cycles */
+const int pzero = 500;      /* cycles */
+const int xyzfrq = 1000;  /* steps */
+const int globfrq = 5;   /* steps */
+const int chkfrq = 5;    /* steps */
+const int tres = 200;
 const int nemerg = NEMERG_c;
-const double tmin = 40.;
-const double tmax = 200.;
+const double tmin[2] = {700.,800.};
+const double tmax[2] = {1000.,970.};
 const double divfac = 0.7;
 const double bfrac = 1.;
+const double spltfac = 0.9;
 
-const double s = 2.e-2;
+const double s = 5.e-3;
 const double frac = 0.1;
 const double wfrac = 1.0;
 const double kappa = 0.1;
 
-const double grav = 0.001;
+const double grav = 0.02;
 const double tstep = 0.01;
 const double beta = 0.0002489;
 
@@ -65,20 +67,20 @@ typedef struct _opoint_t
   double x[NOP_c];
 } opoint_t;
 
-typedef struct _fluxpoint_t
+typedef struct _fluxlist_t
 {
-  double x[NPART_c][NDIM_c];
-  double v[NPART_c][NDIM_c];
-  double f1[NPART_c][NDIM_c];
-  double wt;
-  int from[2];
-  int clock;
-  int mytim;
-} fluxpoint_t ;
+  int full;       /* 0 = not full; 1 = full */
+  int nlist;      /* how many points in the list are used up */
+  double twlist;  /* total weight of flux list */
+  point_t pts[MXLIST_c];  /* the list of flux points for each region */
+  double wlist[MXLIST_c];
+  int from[MXLIST_c][2];
+  int clock[MXLIST_c];
+} fluxlist_t ;
 
 empoint_t emerg[NEMERG_c];
 
 opoint_t basin[2];
 double bwidth = 0.;
-double bmin = 87.;
-double bmax = 115.;
+double bmin = 900;
+double bmax = 960.;
